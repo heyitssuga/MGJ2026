1e346a1bcd5593a44abe2ee363ca5498bf14297a
Allen Krupic <110694062+heyitssuga@users.noreply.github.com>
2026-02-17 16:15:15 -0700

    Gettin animation

---
+++ The Blob Chronicles/Assets/Script/CharacterMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private InputAction Move;
+
+    public int Speed;
+
+    private Vector2 movement;
+
+    public bool jumping;
+
+    private LayerMask groundLayer;
+
+    private Animator _animator;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Move = InputSystem.actions["Move"];
+        Speed = 6;
+        rb.linearVelocity = Vector2.zero;
+        jumping = false;
+        groundLayer =  LayerMask.GetMask("Ground");
+        rb.freezeRotation = true;
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement = Move.ReadValue<Vector2>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit2D hit =  Physics2D.Raycast(transform.position, Vector2.down, 1.5f, groundLayer);
+        
+
+        if (hit)
+        {
+            jumping = false;
+        }
+        else
+        {
+            jumping = true;
+        }
+        if (movement.y > 0 && !jumping)
+        {
+            rb.AddForceY(150);
+        }
+        
+        transform.Translate(new Vector3(movement.x, 0, 0) *  Speed * Time.deltaTime);
+
+        if (movement.x == 0 && !jumping)
+        {
+            _animator.SetBool("Idle", true);
+        }
+        else
+        {
+            _animator.SetBool("Idle", false);
+        }
+        
+        
+        
+        
+
+    }
+}
